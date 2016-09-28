b7ebdf83b423eb1b2524dd81db8ddccb1ae38a5f
Samuel Knox <sam@evorlor.com>
2016-09-28 19:30:57 -0400

    new tilemap explosive platforms

---
+++ Assets/ExplodingPlatform.cs
@@ -0,0 +1,31 @@
+﻿using CreativeSpore.SmartColliders;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class ExplodingPlatform : MonoBehaviour
+{
+    [Tooltip("How long in seconds after player contact until the platform is destroyed")]
+    [SerializeField]
+    [Range(0.0f, 5.0f)]
+    private float explosionTime = 1.0f;
+
+    private Collider2D edge;
+
+    void Awake()
+    {
+        edge = GetComponent<Collider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        var player = collider2D.GetComponent<Player>();
+        if (!player)
+        {
+            return;
+        }
+        if (edge.bounds.center.y <= collider2D.bounds.min.y)
+        {
+            Destroy(gameObject, explosionTime);
+        }
+    }
+}
