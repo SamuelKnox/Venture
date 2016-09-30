17a6f4ee5e647a90fe8c1b6269dec36c12b1620a
Samuel Knox <sam@evorlor.com>
2016-09-30 18:22:43 -0400

    added room listener

---
+++ Assets/Debug/RoomLabeler.cs
@@ -0,0 +1,26 @@
+﻿using LevelGenerator2D;
+using TMPro;
+using UnityEngine;
+
+public class RoomLabeler : MonoBehaviour
+{
+    [Tooltip("Text field used to display room name")]
+    [SerializeField]
+    private TextMeshProUGUI roomNameText;
+
+    void Start()
+    {
+        var level = FindObjectOfType<Level>();
+        if (!level)
+        {
+            Debug.LogError("Could not find level!", gameObject);
+            return;
+        }
+        level.AddListener(this);
+    }
+
+    public void OnRoomEnter(Room room)
+    {
+        roomNameText.text = room.name;
+    }
+}
---
+++ Assets/Scripts/Custom Unity Library/Extensions/C#/ObjectExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace CustomUnityLibrary
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Checks to see if an object contains a method
+        /// </summary>
+        /// <param name="source">Object to check</param>
+        /// <param name="methodName">Name of method</param>
+        /// <returns>Whether or not the object contains the method</returns>
+        public static bool ContainsMethod(this object source, string methodName)
+        {
+            var type = source.GetType();
+            return type.GetMethod(methodName) != null;
+        }
+    }
+}
